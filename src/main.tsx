28d30a7dcc0bcba5ebbde595fd716dec6370bb01
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2026-01-23 16:11:02 +0900

    lint対応

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
-createRoot(document.getElementById("pixi-container")!).render(<App />);
+const pixiContainer = document.getElementById("pixi-container");
+if (!pixiContainer) {
+    throw new Error("Pixi container not found");
+}
+
+createRoot(pixiContainer).render(<App />);
