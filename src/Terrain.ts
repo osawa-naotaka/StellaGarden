aab3101998bbe2430fbaea6fbd042936443ab803
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2026-01-24 23:43:50 +0900

    simplexのスケールを変更

--- src/Terrain.ts
+++ src/Terrain.ts
@@ -4,7 +4,7 @@ import { createNoise2D } from "simplex-noise";
 export function generateTerrain(width: number, height: number) {
     const noise2D = createNoise2D(alea("rand"));
     const terrain: number[][] = [];
-    const scale = 0.1; // スケールを小さくすると大きな地形に
+    const scale = 0.04; // スケールを小さくすると大きな地形に
 
     for (let y = 0; y < height; y++) {
         terrain[y] = [];
